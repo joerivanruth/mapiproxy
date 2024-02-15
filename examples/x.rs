1cb42a3fc11e2809de859c41351758171558baba
Joeri van Ruth <joeri.van.ruth@monetdbsolutions.com>
2024-02-15 12:48:46 +0100

    Check if usage info in README.m is up to date before commit

--- examples/x.rs
+++ examples/x.rs
@@ -1,8 +1,16 @@
-use std::{env, fs};
+//! This is a helper script, not really an example.
+
+use std::{
+    env,
+    fs::{self, File},
+    io::{self, BufRead, BufReader},
+    path::Path,
+};
 
 use anyhow::{bail, Context, Result as AResult};
 
-const VERSION: &str = env!("CARGO_PKG_VERSION");
+use itertools::Itertools;
+use mapiproxy::VERSION;
 
 const USAGE: &str = "\
     Usage: cargo run --example x -- CMD [ARGS]
@@ -10,6 +18,7 @@ const USAGE: &str = "\
         x version [TEMPLATE]        template is prefix or has '@' to be replaced with version
         checktag TAG                check if tag (v1.2.3) matches version number 1.2.3
         relnotes                    extract current entry from CHANGELOG.md
+        checkreadme [--fix]         make sure Usage output in README.md is up to date
 ";
 
 fn main() -> AResult<()> {
@@ -22,6 +31,7 @@ fn main() -> AResult<()> {
         "version" => version(args),
         "checktag" => checktag(args),
         "relnotes" => relnotes(args),
+        "checkreadme" => checkreadme(args),
         _ => bail!("Unknown command: {cmd}\n{USAGE}"),
     }
 }
@@ -58,9 +68,7 @@ fn relnotes(mut args: impl Iterator<Item = String>) -> AResult<()> {
     }
 
     let changelog_file = "CHANGELOG.md";
-    let changelog = fs::read_to_string(changelog_file)
-        .with_context(|| format!("Could not read {changelog_file}"))?;
-    let lines: Vec<_> = changelog.lines().collect();
+    let lines = read_file(changelog_file)?;
 
     let header = format!("## mapiproxy {VERSION} - ");
     let Some(start) = lines.iter().position(|line| line.starts_with(&header)) else {
@@ -77,5 +85,95 @@ fn relnotes(mut args: impl Iterator<Item = String>) -> AResult<()> {
     }
 
     println!("{}", buffer.trim());
+    Ok(())
+}
+
+fn read_file(path: impl AsRef<Path>) -> AResult<Vec<String>> {
+    let path = path.as_ref();
+    let file = File::open(path).with_context(|| format!("Could not open {}", path.display()))?;
+    let br = BufReader::new(file);
+    let content: io::Result<Vec<_>> = br.lines().collect();
+    Ok(content?)
+}
+
+fn read_filex(path: impl AsRef<Path>) -> AResult<String> {
+    let path = path.as_ref();
+    fs::read_to_string(path).with_context(|| format!("Could not read {}", path.display()))
+}
+
+fn checkreadme(mut args: impl Iterator<Item = String>) -> AResult<()> {
+    const README_FILE: &str = "README.md";
+    const START_MARKER: &str = "```plain";
+    const END_MARKER: &str = "```";
+
+    let do_fix = match args.next() {
+        None => false,
+        Some(a) if a == "--fix" => true,
+        Some(other) => bail!("Unknown argument: {other}"),
+    };
+
+    // Parse the existing readme file
+    let old_readme = read_filex(README_FILE)?;
+    let old_lines = old_readme.split_inclusive("\n").collect_vec();
+    let Some(start) = old_lines
+        .iter()
+        .position(|line| line.starts_with(START_MARKER))
+    else {
+        bail!("Could not find {START_MARKER:?} in {old_readme}");
+    };
+    let start = start + 1;
+    let Some(rel_end) = old_lines[start..]
+        .iter()
+        .position(|line| line.starts_with(END_MARKER))
+    else {
+        bail!(
+            "Could not find {END_MARKER:?} in {old_readme} after line {n}",
+            n = start + 1
+        );
+    };
+    let end = start + rel_end;
+
+    let mut old_usage = String::with_capacity(old_readme.len());
+    for line in &old_lines[start..end] {
+        old_usage.push_str(line);
+    }
+
+    let new_usage = mapiproxy::entrypoint::USAGE;
+
+    if old_usage == new_usage {
+        return Ok(());
+    }
+
+    println!("{old_usage:?}");
+
+    println!("Usage information has changed: ");
+    for diff in diff::lines(&old_usage, &new_usage) {
+        match diff {
+            diff::Result::Left(line) => println!("-{line}"),
+            diff::Result::Both(line, _) => println!(" {line}"),
+            diff::Result::Right(line) => println!("+{line}"),
+        }
+    }
+
+    if !do_fix {
+        bail!("Usage info in {README_FILE} does not match --help output");
+    }
+
+    write_file(
+        README_FILE,
+        &[&old_lines[..start], &[new_usage], &old_lines[end..]],
+    )
+}
+
+fn write_file(path: impl AsRef<Path>, content: &[&[&str]]) -> AResult<()> {
+    let path = path.as_ref();
+    let mut text = String::new();
+    for chunk in content {
+        for line in *chunk {
+            text.push_str(line);
+        }
+    }
+
+    fs::write(path, text).with_context(|| format!("Could not write {}", path.display()))?;
     Ok(())
 }
